f2390d3f209c83ee7346f54b5968d411c37f1008
Steve Giordano <steve1nyc@gmail.com>
2024-08-17 19:08:16 -0400

    Move scripts to the scripts folder

--- scripts/spike.gd
+++ scripts/spike.gd
@@ -1,10 +1,4 @@
 extends Node2D
 
-@onready var ball = $"../Ball"
-
-
-
-
-
-func _on_collision_shape_2d_area_entered(area):
-	print("aszfghncxjinujhardgbui fbhuyhjf dgb nhhe fvnhjs vnbbh gfvdsnh fv hdfyvbsdmvmjnuser gjhy vm jnd bh dfn dsjmfd ,kbgxuyjh jrydxjhbvf scujh bhfhigubhhj g")
+func _on_area_2d_body_entered(body):
+	body.SpikeHit()
