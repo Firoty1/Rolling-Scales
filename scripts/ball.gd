aa8e8b2bf23e5c9eeaf333165ca74998d6a29857
Firoty1 <ultrapokegamer55@gmail.com>
2024-08-18 15:31:40 -0400

    ai gbi

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -9,22 +9,24 @@ var is_dead = false
 func _input(event):
 	if is_dead == false:
 		if event.is_action_pressed("shrink"):
-			sprite_2d.scale.x = sprite_2d.scale.x * .9
-			sprite_2d.scale.y = sprite_2d.scale.y * .9
-			
-			collision_shape_2d.scale.x = collision_shape_2d.scale.x * .9
-			collision_shape_2d.scale.y = collision_shape_2d.scale.y * .9
-			
-			size = size - 1
-			
+			if size > -2:
+				sprite_2d.scale.x = sprite_2d.scale.x * .5
+				sprite_2d.scale.y = sprite_2d.scale.y * .5
+				
+				collision_shape_2d.scale.x = collision_shape_2d.scale.x * .5
+				collision_shape_2d.scale.y = collision_shape_2d.scale.y * .5
+				
+				size = size - 1
+				
 		if event.is_action_pressed("grow"):
-			sprite_2d.scale.x = sprite_2d.scale.x * 1.1
-			sprite_2d.scale.y = sprite_2d.scale.y * 1.1
-			
-			collision_shape_2d.scale.x = collision_shape_2d.scale.x * 1.1
-			collision_shape_2d.scale.y = collision_shape_2d.scale.y * 1.1
-			
-			size = size + 1
+			if size < 2:
+				sprite_2d.scale.x = sprite_2d.scale.x * 1.5
+				sprite_2d.scale.y = sprite_2d.scale.y * 1.5
+				
+				collision_shape_2d.scale.x = collision_shape_2d.scale.x * 1.5
+				collision_shape_2d.scale.y = collision_shape_2d.scale.y * 1.5
+				
+				size = size + 1
 		
 		if event.is_action_pressed("turbo"):
 			var movement_direction = self.linear_velocity.normalized()
