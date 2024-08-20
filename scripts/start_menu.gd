6c3cf6087fd633921d4015dc726a7a39f00b0ee9
Steve Giordano <steve1nyc@gmail.com>
2024-08-20 11:02:21 -0400

    Add rolling cheese to start menu

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -7,6 +7,10 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start_button.grab_focus()
+	
+func _process(delta):
+	$Path2D/PathFollow2D.progress += 300 * delta
+	$Path2D/PathFollow2D.rotation += 4 * delta
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
